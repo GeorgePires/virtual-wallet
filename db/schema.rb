618e7cd15d5666ae1885c047770e65e93f5fd9e9
GeorgePires <georgeasp.pi@gmail.com>
2023-02-24 20:11:55 -0300

    :boom: fix: remove attributes first_name and last_name from admin

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_222249) do
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
