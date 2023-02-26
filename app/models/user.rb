38a82eef7328368ba737f2b04e866e5967aad0ae
GeorgePires <georgeasp.pi@gmail.com>
2023-02-25 22:05:23 -0300

    :sparkles: feat: callback  create_wallet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  after_create :create_wallet
+  
+  has_one :wallet
+
+  validates :name, :email, presence: true
+  
+  def create_wallet
+    wallet_number = (Digest::SHA1.hexdigest(SecureRandom.uuid)[0..7]).upcase
+    unless Wallet.exists?(wallet_number: wallet_number)
+      Wallet.create!(user_id: self.id, balance: 0.00, wallet_number: wallet_number)
+    end
+  end
 end
