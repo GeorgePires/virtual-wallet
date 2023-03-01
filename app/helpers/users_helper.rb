1dc573daa793fdf9cc1bbd6c83d122a4d1f25f5b
GeorgePires <georgeasp.pi@gmail.com>
2023-03-01 14:10:58 -0300

    :sparkles: feat: create helper color_transaction and scopes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  def color_transaction(transaction_type)
+    color = transaction_type == 'credit' ? 'green' : 'red'
+    "px-2 py-1 font-semibold leading-tight text-#{color}-700 bg-#{color}-100 rounded-full dark:text-#{color}-100 dark:bg-#{color}-700"
+  end
 end
